05e473b5a98ce03fffbc544b19ff8140a4be3494
cooi123 <calebooi@hotmail.com>
2025-04-30 00:58:06 +1000

    update celery config

--- celery_worker.py
+++ celery_worker.py
@@ -1,11 +1,14 @@
+import sys
 from celery import Celery
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
+sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
 # Create Celery instance
 app = Celery('ed_summarizer',
              broker=os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0'),
              backend=os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'))
 app.conf.imports = (['app.tasks.fetch_insert_to_vector_db_tasks',
                     'app.tasks.thread_clustering_tasks'])
+if __name__ == "__main__":
+    app.worker_main(['worker', '--loglevel=info'])
