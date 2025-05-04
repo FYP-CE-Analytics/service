a73c11de952cb60dda4f3c1243dde384921bee10
cooi123 <calebooi@hotmail.com>
2025-05-04 23:15:28 +1000

    task schema

---
+++ app/schemas/tasks/storing_task_schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from app.schemas.tasks.base_task_result_schema import TaskResult
+
+
+class StoringTaskIds(BaseModel):
+    """
+    Schema for the result of a storing task.
+    """
+    unit_ids: list[str] = Field(...,
+                                description="List of unit IDs processed in the task")
+
+
+StoringTaskResult = TaskResult[StoringTaskIds]
